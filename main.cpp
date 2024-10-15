f7260ddf072f6cae9a99ff64817f85ad5556c639
BrunoHoffmann15 <bruno.siqueira.hoffmann@gmail.com>
2024-10-14 22:24:50 -0300

    feat: muda nome do título

--- main.cpp
+++ main.cpp
@@ -130,7 +130,7 @@ int main(){
 #endif
 
     // Criação da janela GLFW
-    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Spaceship Game!", nullptr, nullptr);
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Spaceship Meteor", nullptr, nullptr);
     glfwMakeContextCurrent(window);
 
     // Fazendo o registro da função de callback para a janela GLFW
